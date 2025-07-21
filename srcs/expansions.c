5eb3212cfa23b606157b456949f3956edad4958e
Reyyyss <hcr2207@gmail.com>
2025-07-21 14:35:58 +0100

    expansion tratadas

--- srcs/expansions.c
+++ srcs/expansions.c
@@ -6,7 +6,7 @@
 /*   By: hcarrasq <hcarrasq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 11:50:34 by hcarrasq          #+#    #+#             */
-/*   Updated: 2025/07/16 16:03:35 by hcarrasq         ###   ########.fr       */
+/*   Updated: 2025/07/21 11:05:26 by hcarrasq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char *expand_argument(const char *arg)
 	bool in_double_quotes = false;
 	char *result = calloc(1, 1);
 
+	if (!result)
+		return (NULL);
 	while (arg[i])
 	{
 		if (arg[i] == '\'' && !in_double_quotes)
@@ -41,16 +43,16 @@ char *expand_argument(const char *arg)
 			if (arg[i] == '?')
 			{
 				char *status_str = ft_itoa(1);// depois adicionar a funcao de ober o exit status
-				if (!status_str)
+				if (!status_str || strncat_realloc(&result, status_str, strlen(status_str)) == -1)
 				{
+					free(status_str);
 					free(result);
 					return NULL;
 				}
-				strncat_realloc(&result, status_str, strlen(status_str));
 				free(status_str);
 				i++;
 			}
-			else if (ft_isalpha(arg[i]) || arg[i] == '_')
+			else if (arg[i] && (ft_isalpha(arg[i]) || arg[i] == '_'))
 			{
 				int start = i;
 				while (ft_isalnum(arg[i]) || arg[i] == '_')
@@ -68,11 +70,21 @@ char *expand_argument(const char *arg)
 					strncat_realloc(&result, value, strlen(value));
 			}
 			else
-				strncat_realloc(&result, "$", 1);
+			{
+				if (strncat_realloc(&result, "$", 1) == -1)
+				{
+					free(result);
+					return (NULL);
+				}
+			}
 		}
 		else
 		{
-			strncat_realloc(&result, &arg[i], 1);
+			if (strncat_realloc(&result, &arg[i], 1) == -1)
+			{
+				free(result);
+				return (NULL);
+			}
 			i++;
 		}
 	}
@@ -82,7 +94,10 @@ char *expand_argument(const char *arg)
 void expansion_trade(void)
 {
 	t_command *cmd = prog_data()->commands;
-	while (cmd)
+	int	i;
+
+	i = 0;
+	while (cmd && cmd->args[i])
 	{
 		for (int i = 0; cmd->args && cmd->args[i]; i++)
 		{
